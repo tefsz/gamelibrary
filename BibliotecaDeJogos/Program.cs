4478397d4334428b5209301d2c6aa6b988c96b2e
tefsz <stefaniebarbosa234@gmail.com>
2021-09-15 02:21:23 -0300

    Enviando arquivos iniciais

---
+++ BibliotecaDeJogos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BibliotecaDeJogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("##### - BIBLIOTECA DE JOGOS  #####");
+            Console.WriteLine("\n");
+            Console.WriteLine("1- Adicionar um jogo");
+            Console.WriteLine("2- Listar os Jogos");
+            Console.WriteLine("3- Atualizar um jogo");
+            Console.WriteLine("4- Deletar um jogo");
+        }
+    }
+}
